5d144bf094b5bb8820506a858da31e185a373a12
Caspar <111374516+cheesynob39@users.noreply.github.com>
2023-03-11 19:25:01 +0000

    Main --> Loadstring
     -

--- LOADSTRING.lua
+++ LOADSTRING.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/cheesynob39/R2O/main/Main.lua"))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/cheesynob39/R2O/main/Main.lua")))()
