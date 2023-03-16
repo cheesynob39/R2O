4912f4aa8c7a6eaf40d0667ebb57d26d50f82070
Caspar <111374516+cheesynob39@users.noreply.github.com>
2023-03-16 20:47:04 +0000

    Updated

--- Main.lua
+++ Main.lua
@@ -11,5 +11,20 @@ Method = "GET"
 if Request.SuccessCode ~= 404 then
 
    loadstring(Link.Body)()
+    
+else
+    
+    if Request.SuccessCode == 404 and game.PlaceId == 11520107397 or game.PlaceId == 9015014224 then
+        
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/cheesynob39/R2O/main/Games/6403373529.lua"))()
+        
+    else
+        
+        error("GAME NOT SUPPORTED WITH R2O")
+
+    end
+    
 
 end
+
+
