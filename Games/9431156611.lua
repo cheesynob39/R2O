0028d6dc0f710d24ef454f554e8d95875151b42f
Caspar <111374516+cheesynob39@users.noreply.github.com>
2023-03-16 21:37:10 +0000

    Update 9431156611.lua

--- Games/9431156611.lua
+++ Games/9431156611.lua
@@ -14,28 +14,20 @@ if game.CoreGui:FindFirstChild("CombatTab", 1) then
       else
     
           getgenv().Running = false
-     
+  
      end
 
     
-    if getgenv().Running  then
+if getgenv().Running  then
         
              
-		error("R2O Was Found Running")
+	error("R2O Was Found Running")
 		
 		
-        else
+    else
 
-	_G.settingsTable = {
-
-		Speed = 20.554494,
-
-		Jump = 50.554494, 
-    
-	}
-
-
-      local decal_codes = {
+    local decal_codes = {
+        
     ["http://www.roblox.com/asset/?id=9648769161"] = "4",
 
     ["http://www.roblox.com/asset/?id=9648765536"] = "2",
@@ -69,26 +61,15 @@ if game.CoreGui:FindFirstChild("CombatTab", 1) then
     ["http://www.roblox.com/asset/?id=9648712563"] = "2"
 }
 
+    local Items = {"Potion of Strength", "Bull's essence", "Speed Potion", "Boba", "Frog Potion", "Strength Brew", "Frog Brew", "Speed Brew"}
 
-local Items = {
-
-  "Potion of Strength", 
-  "Bull's essence", 
-  "Speed Potion", 
-  "Boba", 
-  "Frog Potion", 
-  "Strength Brew", 
-  "Frog Brew", 
-  "Speed Brew"
-
-}
+    local Lplr = game.Players.LocalPlayer
 
 	local Time = tick()
+	
+	local Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/cheesynob39/R20-EXPLOITER/main/Files/Functions.lua"))()
 
-	local userinputservice = game:GetService("UserInputService")
-
-	local Coasting =
-	loadstring(game:HttpGet(("https://raw.githubusercontent.com/cheesynob39/Coasting/main/Source.lua")))()
+	local Coasting = loadstring(game:HttpGet(("https://raw.githubusercontent.com/cheesynob39/Coasting/main/Source.lua")))()
 
     local namecall
     namecall = hookmetamethod(game, "__namecall", function(self, ...)
@@ -106,153 +87,161 @@ local Items = {
     end)
 
 	local CUMBAT = Coasting:CreateTab("Combat")
-
 	local CUMBAT3 = CUMBAT:CreateSection("Slaps")
-
 	local CUMBAT2 = CUMBAT:CreateSection("Config")
 
 	--------------------------------------------
 
 	local MISCUM = Coasting:CreateTab("Misc")
-
 	local MISCUM2 = MISCUM:CreateSection("Badges")
-
 	local MISCUM3 = MISCUM:CreateSection("Items")
 
 	--------------------------------------------
 
 	local Game = Coasting:CreateTab("Game")
-
 	local Game1 = Game:CreateSection("Settings")
-
 	local Game2 = Game:CreateSection("Map")
 
 	--------------------------------------------
 
 	local Local = Coasting:CreateTab("Local")
-
 	local Local1 = Local:CreateSection("WS")
-
 	local Local2 = Local:CreateSection("JP")
-
+	
 	--------------------------------------------
 	
 	local Credits = Coasting:CreateTab("Credits")
-
 	local Creators = Credits:CreateSection("Creators")
-
 	local Discord = Credits:CreateSection("Discord")
 
         --------------------------------------------
 
-	Game2:CreateButton("Anti Acid", function()
+	Game2:CreateToggle("Anti Acid", function(bool)
 		
-        local Anti_Acid = Instance.new("Part", workspace)
+        if bool == true then
+            
+        game.Workspace:FindFirstChild("antiAcid").CanCollide = true
 
-		Anti_Acid.Size = Vector3.new(154, 26, 132)
-
-		Anti_Acid.Position = Vector3.new(-60, -5, -731)
-
-		Anti_Acid.Transparency = 1
-
-		Anti_Acid.Anchored = true
+        else
+            
+        game.Workspace:FindFirstChild("antiAcid").CanCollide = false
+            
+        end
 
 	end)
 
 
 	MISCUM2:CreateButton("Get Chain", function()
+    
+    for i,v in ipairs(game:GetService("Workspace").Map:WaitForChild("CodeBrick"):WaitForChild("SurfaceGui"):GetDescendants()) do
+        
+        if v.Name == 'IMGTemplate' then
+            
+            local decal_url = tostring(v.Image)
+            local code = decal_codes[decal_url]
 
+            if 0 < #game:GetService("Workspace").Map:WaitForChild("OriginOffice").Door.Keypad.Display.SurfaceGui.TextLabel.Text then
+                fireclickdetector(game:GetService("Workspace").Map:WaitForChild("OriginOffice").Door.Keypad.Buttons.Reset.ClickDetector)
+            end
 
-for i,v in ipairs(game:GetService("Workspace").Map:WaitForChild("CodeBrick"):WaitForChild("SurfaceGui"):GetDescendants()) do
-    if v.Name == 'IMGTemplate' then
-        local decal_url = tostring(v.Image)
-        local code = decal_codes[decal_url]
+            task.wait(.2)
 
-        if 0 < #game:GetService("Workspace").Map:WaitForChild("OriginOffice").Door.Keypad.Display.SurfaceGui.TextLabel.Text then
-            fireclickdetector(game:GetService("Workspace").Map:WaitForChild("OriginOffice").Door.Keypad.Buttons.Reset.ClickDetector)
+            fireclickdetector(game:GetService("Workspace").Map:WaitForChild("OriginOffice").Door.Keypad.Buttons[code].ClickDetector)
         end
-
-        task.wait(.2)
-
-        fireclickdetector(game:GetService("Workspace").Map:WaitForChild("OriginOffice").Door.Keypad.Buttons[code].ClickDetector)
     end
-end
 
-task.wait(.2)
+    task.wait(.2)
 
-fireclickdetector(game:GetService("Workspace").Map:WaitForChild("OriginOffice").Door.Keypad.Buttons.Enter.ClickDetector)
+    fireclickdetector(game:GetService("Workspace").Map:WaitForChild("OriginOffice").Door.Keypad.Buttons.Enter.ClickDetector)
 
     end)
 
     MISCUM3:CreateButton("Get All Items", function()
-        local Lplr = game.Players.LocalPlayer
+
         if Lplr.Character.inMatch.Value and workspace:FindFirstChildWhichIsA("Tool") then
+            
             for _, Items in ipairs(game.Workspace:GetDescendants()) do
+                
                 if Items:IsA("TouchTransmitter") and Items.Parent.Name == "Handle" then
                     Lplr.Character.Humanoid:EquipTool(Items.Parent.Parent)
                 end
             end
+            
             Lplr.Character.Humanoid:UnequipTools()
             
             else
-                
+        
                 print("No Items")
+                
         end
     end)
 
 
 
     MISCUM3:CreateButton("Use All Permanent Items", function()
-            for _, L_3 in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
-                if L_3:IsA("Tool") and table.find(Items, L_3.Name) then
-                    L_3.Parent = game.Players.LocalPlayer.Character
-                end
+            
+        for _, L_3 in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
+                
+            if L_3:IsA("Tool") and table.find(Items, L_3.Name) then
+                L_3.Parent = Lplr.Character
             end
-            wait(.5)
-            for _, L_4 in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+                
+        end
+            
+            task.wait(.3)
+            
+            for _, L_4 in pairs(Lplr.Character:GetChildren()) do
+                
                 if L_4:IsA("Tool") then
                     L_4:Activate()
                 end
+                
             end
     end)
     
     MISCUM3:CreateButton("Bomb Bus", function()
-    
-        local lp = game.Players.LocalPlayer
+        
 			for _, L_5 in ipairs(game.Workspace:GetDescendants()) do
+				
 				if L_5:IsA("TouchTransmitter") and L_5.Parent.Name == "Handle" and L_5.Parent.Parent.Name == "Bomb" and workspace:FindFirstChild("BusModel") then
-
-    
-    
-					lp.Character.Humanoid:EquipTool(L_5.Parent.Parent)
+					Lplr.Character.Humanoid:EquipTool(L_5.Parent.Parent)
 				end
+				
 			end
+			
 			wait(.1)
+			
 			for _, L_6 in pairs(lp.Backpack:GetChildren()) do
+				
 				if L_6:IsA("Tool") and L_6.Name == "Bomb" then
-					L_6.Parent = lp.Character
+					L_6.Parent = Lplr.Character
 				end
+				
 			end
+			
 			wait(.25)
-			for _, L_7 in pairs(lp.Character:GetChildren()) do
+			
+			for _, L_7 in pairs(Lplr.Character:GetChildren()) do
+				
 				if L_7:IsA("Tool") and L_7.Name == "Bomb" then
 					L_7:Activate()
 				end
+				
 			end
 			
     end)
 
-    Game2:CreateButton("Anti Lava", function(AntiLava)
-
-        local AntiLava = Instance.new("Part", game.Workspace)
-
-	    AntiLava.Position = Vector3.new(-238, -43, 401)
-
-	    AntiLava.Size = Vector3.new(150,10,150)
-
-	    AntiLava.Anchored = true
-
-	    AntiLava.Transparency = 1
+    Game2:CreateToggle("Anti Lava", function(bool)
+    
+    if bool == true then
+        
+        game.workspace:FindFirstChild("antiLava").CanCollide = true
+        
+    else
+        
+        game.workspace:FindFirstChild("antiLava").CanCollide = true
+        
+    end
 
     end)
 
@@ -261,7 +250,7 @@ fireclickdetector(game:GetService("Workspace").Map:WaitForChild("OriginOffice").
     Game1:CreateToggle("Remove Zone Blur", function(Blur)
     
     if Blur == true then
-                    
+        
 	game:GetService("ReplicatedStorage").ZoneEffects.ZoneBlur.Enabled = false
 
 	game:GetService("ReplicatedStorage").ZoneEffects.ZoneColorCorrection.Enabled = false
@@ -276,46 +265,48 @@ fireclickdetector(game:GetService("Workspace").Map:WaitForChild("OriginOffice").
     
     end)
 
-
-
     CUMBAT3:CreateToggle("Slap Aura",function(bool)
-				getgenv().Aura = bool
-				while getgenv().Aura == true do
-					task.wait()
-					for _, L_8 in pairs(game.Players:GetPlayers()) do
-						if L_8 ~= game.Players.LocalPlayer then
-							if L_8.Character:FindFirstChild("HumanoidRootPart") ~= nil and L_8.Character:FindFirstChild("Humanoid") and not L_8.Character:FindFirstChild("Dead")  and not game.Players.LocalPlayer.Character:FindFirstChild("Dead")  then
-								local magnitude =
-								(L_8.Character.HumanoidRootPart.Position -
-								game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
-								if 20 >= magnitude  then
+		  slapAura = bool
+		  
+		  if bool == true then
+		      
+		    while slapAura == true do
+					
+				task.wait()
+					
+				for _, L_8 in pairs(game.Players:GetPlayers()) do
+						
+					if L_8 ~= Lplr then
+						
+						if L_8.Character:FindFirstChild("HumanoidRootPart") ~= nil and L_8.Character:FindFirstChild("Humanoid") and not L_8.Character:FindFirstChild("Dead")  and not Lplr.Character:FindFirstChild("Dead")  then
+								
+							local magnitude = (L_8.Character.HumanoidRootPart.Position - Lplr.Character.HumanoidRootPart.Position).Magnitude
+						
+								if 20 >= magnitude then
 									game:GetService("ReplicatedStorage").Events.Slap:FireServer(L_8.Character.Torso)
 								end
+								
 							end
 						end
 					end
-				end
-			end)
+		    end
+			
+		  end
+		  
+		end)
 
-
-
-    Local1:CreateSlider("Walkspeed", 20, 300, 20, false, function(JP)
+    Local1:CreateSlider("Walkspeed", 20, 300, 20, false, function(WS)
 	
-    _G.settingsTable.Speed = JP
-	
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = _G.settingsTable.Speed + 0.554494
+    Lplr.Character.Humanoid.WalkSpeed = WS
    
     end)
 
-    Local2:CreateSlider("Jump Power", 50, 500, 50, false, function(WS)
+    Local2:CreateSlider("Jump Power", 50, 500, 50, false, function(JP)
     
-        _G.settingsTable.Jump = WS
-    
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = _G.settingsTable.Jump
+        Lplr.Character.Humanoid.JumpPower = JP
         
     end)
 	
-   
    Creators:CreateButton("Anakin#3568",function()
        
        setclipboard("Anakin#3568")
@@ -327,22 +318,38 @@ fireclickdetector(game:GetService("Workspace").Map:WaitForChild("OriginOffice").
        setclipboard("RealIced#0001")
        
    end)
-
    
    Discord:CreateButton("Copy Invite",function()
        
        setclipboard("https://discord.gg/zty372wma5")
        
    end)
-
-
-		
+    
+    --------------------------------------------
+    
+    local antiLava = Instance.new("Part", workspace)
+    antiLava.Name = "antiLava"
+	antiLava.Position = Vector3.new(-238, -43, 401)
+	antiLava.Size = Vector3.new(150,10,150)
+	antiLava.Anchored = true
+	antiLava.Transparency = 1
+	antiLava.CanCollide = false
+	
+	local antiAcid = Instance.new("Part", workspace)
+	antiAcid.Name = "antiAcid"
+	antiAcid.Size = Vector3.new(154, 26, 132)
+	antiAcid.Position = Vector3.new(-60, -5, -731)
+	antiAcid.Transparency = 1
+	antiAcid.CanCollide = false
+	antiAcid.Anchored = true
+	
+	--------------------------------------------
+	
+	shared.removeBlue()
+	
     game:GetService("SoundService").KillSound.SoundId = "rbxassetid://2440889605" -- Sound Effect
-
-            
+     
 	print("R2O v1.23 Took " .. tick() - Time .. " Seconds To Load ") -- Loading Time
-
-
 
 end
 
