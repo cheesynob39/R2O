db48d9915924ded2b6464bde3d97508707431250
Caspar <111374516+cheesynob39@users.noreply.github.com>
2023-03-16 20:42:56 +0000

    Working

--- Main.lua
+++ Main.lua
@@ -1,1 +1,15 @@
+local Requests = request or http and http_request or syn and syn.request
 
+local Link = Requests({
+
+Url = "https://raw.githubusercontent.com/cheesynob39/R2O/main/Games/".. game.PlaceId.. ".lua",
+    
+Method = "GET"
+
+)}
+
+if Request.SuccessCode ~= 404 then
+
+   loadstring(Link.Body)()
+
+end
