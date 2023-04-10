a3e30e6a7e0633ece9e3b5e932b710a2e052735c
Caspar <111374516+cheesynob39@users.noreply.github.com>
2023-04-10 22:12:40 +0100

    Minor Fix

--- Files/freeAnimations.lua
+++ Files/freeAnimations.lua
@@ -32,17 +32,18 @@ game.Players.LocalPlayer.CharacterAdded:Connect(function()
     
 end)
 
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
 
-game.Players.LocalPlayer.Character.Humanoid.Health = 0
-
-repeat task.wait()
+    repeat task.wait()
     
-until game.Players.LocalPlayer.Character ~= nil
+    until game.Players.LocalPlayer.Character ~= nil
 
 game.Players.LocalPlayer.Chatted:Connect(function(msg)
         if string.lower(msg) == "/e l" then
             if Anims["L"].IsPlaying or Anims["Groove"].IsPlaying or Anims["Helicopter"].IsPlaying or Anims["Floss"].IsPlaying or Anims["Kick"].IsPlaying or Anims["Headless"].IsPlaying or Anims["Laugh"].IsPlaying or Anims["Parker"].IsPlaying or Anims["Thriller"].IsPlaying or Anims["Spasm"].IsPlaying then
                 Anims["L"]:Stop(); Anims["Groove"]:Stop(); Anims["Helicopter"]:Stop(); Anims["Floss"]:Stop(); Anims["Kick"]:Stop(); Anims["Headless"]:Stop(); Anims["Laugh"]:Stop(); Anims["Parker"]:Stop(); Anims["Thriller"]:Stop(); Anims["Spasm"]:Stop()
+                task.wait()
+                Anims["L"]:Play()
             else
                 Anims["L"]:Play()
                 ogP = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
@@ -50,6 +51,8 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
         elseif string.lower(msg) == "/e groove" then
             if Anims["L"].IsPlaying or Anims["Groove"].IsPlaying or Anims["Helicopter"].IsPlaying or Anims["Floss"].IsPlaying or Anims["Kick"].IsPlaying or Anims["Headless"].IsPlaying or Anims["Laugh"].IsPlaying or Anims["Parker"].IsPlaying or Anims["Thriller"].IsPlaying or Anims["Spasm"].IsPlaying then
                 Anims["L"]:Stop(); Anims["Groove"]:Stop(); Anims["Helicopter"]:Stop(); Anims["Floss"]:Stop(); Anims["Kick"]:Stop(); Anims["Headless"]:Stop(); Anims["Laugh"]:Stop(); Anims["Parker"]:Stop(); Anims["Thriller"]:Stop(); Anims["Spasm"]:Stop()
+                task.wait()
+                Anims["Groove"]:Play()
             else
                 Anims["Groove"]:Play()
                 ogP = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
@@ -57,6 +60,8 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
         elseif string.lower(msg) == "/e helicopter" then
             if Anims["L"].IsPlaying or Anims["Groove"].IsPlaying or Anims["Helicopter"].IsPlaying or Anims["Floss"].IsPlaying or Anims["Kick"].IsPlaying or Anims["Headless"].IsPlaying or Anims["Laugh"].IsPlaying or Anims["Parker"].IsPlaying or Anims["Thriller"].IsPlaying or Anims["Spasm"].IsPlaying then
                 Anims["L"]:Stop(); Anims["Groove"]:Stop(); Anims["Helicopter"]:Stop(); Anims["Floss"]:Stop(); Anims["Kick"]:Stop(); Anims["Headless"]:Stop(); Anims["Laugh"]:Stop(); Anims["Parker"]:Stop(); Anims["Thriller"]:Stop(); Anims["Spasm"]:Stop()
+                task.wait()
+                Anims["Helicopter"]:Play()
             else
                 Anims["Helicopter"]:Play()
                 ogP = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
@@ -64,6 +69,8 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
         elseif string.lower(msg) == "/e floss" then
             if Anims["L"].IsPlaying or Anims["Groove"].IsPlaying or Anims["Helicopter"].IsPlaying or Anims["Floss"].IsPlaying or Anims["Kick"].IsPlaying or Anims["Headless"].IsPlaying or Anims["Laugh"].IsPlaying or Anims["Parker"].IsPlaying or Anims["Thriller"].IsPlaying or Anims["Spasm"].IsPlaying then
                 Anims["L"]:Stop(); Anims["Groove"]:Stop(); Anims["Helicopter"]:Stop(); Anims["Floss"]:Stop(); Anims["Kick"]:Stop(); Anims["Headless"]:Stop(); Anims["Laugh"]:Stop(); Anims["Parker"]:Stop(); Anims["Thriller"]:Stop(); Anims["Spasm"]:Stop()
+                task.wait()
+                Anims["Floss"]:Play()
             else
                 Anims["Floss"]:Play()
                 ogP = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
@@ -71,6 +78,8 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
         elseif string.lower(msg) == "/e kick" then
             if Anims["L"].IsPlaying or Anims["Groove"].IsPlaying or Anims["Helicopter"].IsPlaying or Anims["Floss"].IsPlaying or Anims["Kick"].IsPlaying or Anims["Headless"].IsPlaying or Anims["Laugh"].IsPlaying or Anims["Parker"].IsPlaying or Anims["Thriller"].IsPlaying or Anims["Spasm"].IsPlaying then
                 Anims["L"]:Stop(); Anims["Groove"]:Stop(); Anims["Helicopter"]:Stop(); Anims["Floss"]:Stop(); Anims["Kick"]:Stop(); Anims["Headless"]:Stop(); Anims["Laugh"]:Stop(); Anims["Parker"]:Stop(); Anims["Thriller"]:Stop(); Anims["Spasm"]:Stop()
+                task.wait()
+                Anims["Kick"]:Play()
          else
                 Anims["Kick"]:Play()
                 ogP = game.Players.LocalPlayer.Character.HumanoidRootPart.Position            
@@ -78,6 +87,8 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
         elseif string.lower(msg) == "/e headless" then
             if Anims["L"].IsPlaying or Anims["Groove"].IsPlaying or Anims["Helicopter"].IsPlaying or Anims["Floss"].IsPlaying or Anims["Kick"].IsPlaying or Anims["Headless"].IsPlaying or Anims["Laugh"].IsPlaying or Anims["Parker"].IsPlaying or Anims["Thriller"].IsPlaying or Anims["Spasm"].IsPlaying then
                 Anims["L"]:Stop(); Anims["Groove"]:Stop(); Anims["Helicopter"]:Stop(); Anims["Floss"]:Stop(); Anims["Kick"]:Stop(); Anims["Headless"]:Stop(); Anims["Laugh"]:Stop(); Anims["Parker"]:Stop(); Anims["Thriller"]:Stop(); Anims["Spasm"]:Stop()
+                task.wait()
+                Anims["Headless"]:Play()
             else
                 Anims["Headless"]:Play()
                 ogP = game.Players.LocalPlayer.Character.HumanoidRootPart.Position   
@@ -85,6 +96,8 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
         elseif string.lower(msg) == "/e rofl" then
                 if Anims["L"].IsPlaying or Anims["Groove"].IsPlaying or Anims["Helicopter"].IsPlaying or Anims["Floss"].IsPlaying or Anims["Kick"].IsPlaying or Anims["Headless"].IsPlaying or Anims["Laugh"].IsPlaying or Anims["Parker"].IsPlaying or Anims["Thriller"].IsPlaying or Anims["Spasm"].IsPlaying then
                 Anims["L"]:Stop(); Anims["Groove"]:Stop(); Anims["Helicopter"]:Stop(); Anims["Floss"]:Stop(); Anims["Kick"]:Stop(); Anims["Headless"]:Stop(); Anims["Laugh"]:Stop(); Anims["Parker"]:Stop(); Anims["Thriller"]:Stop(); Anims["Spasm"]:Stop()
+                task.wait()
+                Anims["Laugh"]:Play()
             else
                 Anims["Laugh"]:Play()
                 ogP = game.Players.LocalPlayer.Character.HumanoidRootPart.Position   
@@ -93,6 +106,8 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
         elseif string.lower(msg) == "/e parker" then
             if Anims["L"].IsPlaying or Anims["Groove"].IsPlaying or Anims["Helicopter"].IsPlaying or Anims["Floss"].IsPlaying or Anims["Kick"].IsPlaying or Anims["Headless"].IsPlaying or Anims["Laugh"].IsPlaying or Anims["Parker"].IsPlaying or Anims["Thriller"].IsPlaying or Anims["Spasm"].IsPlaying then
                 Anims["L"]:Stop(); Anims["Groove"]:Stop(); Anims["Helicopter"]:Stop(); Anims["Floss"]:Stop(); Anims["Kick"]:Stop(); Anims["Headless"]:Stop(); Anims["Laugh"]:Stop(); Anims["Parker"]:Stop(); Anims["Thriller"]:Stop(); Anims["Spasm"]:Stop()
+                task.wait()
+                Anims["Parker"]:Play()
             else
                 Anims["Parker"]:Play()
                 ogP = game.Players.LocalPlayer.Character.HumanoidRootPart.Position   
@@ -100,6 +115,9 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
         elseif string.lower(msg) == "/e thriller" then
             if Anims["L"].IsPlaying or Anims["Groove"].IsPlaying or Anims["Helicopter"].IsPlaying or Anims["Floss"].IsPlaying or Anims["Kick"].IsPlaying or Anims["Headless"].IsPlaying or Anims["Laugh"].IsPlaying or Anims["Parker"].IsPlaying or Anims["Thriller"].IsPlaying or Anims["Spasm"].IsPlaying then
                 Anims["L"]:Stop(); Anims["Groove"]:Stop(); Anims["Helicopter"]:Stop(); Anims["Floss"]:Stop(); Anims["Kick"]:Stop(); Anims["Headless"]:Stop(); Anims["Laugh"]:Stop(); Anims["Parker"]:Stop(); Anims["Thriller"]:Stop(); Anims["Spasm"]:Stop()
+                task.wait()
+                Anims["Thriller"]:Play()
+
             else
                 Anims["Thriller"]:Play()
                 ogP = game.Players.LocalPlayer.Character.HumanoidRootPart.Position   
@@ -107,6 +125,9 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
         elseif string.lower(msg) == "/e spasm" then
             if Anims["L"].IsPlaying or Anims["Groove"].IsPlaying or Anims["Helicopter"].IsPlaying or Anims["Floss"].IsPlaying or Anims["Kick"].IsPlaying or Anims["Headless"].IsPlaying or Anims["Laugh"].IsPlaying or Anims["Parker"].IsPlaying or Anims["Thriller"].IsPlaying or Anims["Spasm"].IsPlaying then
                 Anims["L"]:Stop(); Anims["Groove"]:Stop(); Anims["Helicopter"]:Stop(); Anims["Floss"]:Stop(); Anims["Kick"]:Stop(); Anims["Headless"]:Stop(); Anims["Laugh"]:Stop(); Anims["Parker"]:Stop(); Anims["Thriller"]:Stop(); Anims["Spasm"]:Stop()
+                task.wait()
+                Anims["Spasm"]:Play()
+
             else
                 Anims["Spasm"]:Play()
                 ogP = game.Players.LocalPlayer.Character.HumanoidRootPart.Position   
