60a26bccabf7b0684ba65a1c8e959f3edf8d1e3e
Caspar <111374516+cheesynob39@users.noreply.github.com>
2023-07-12 23:53:15 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,30 +1,18 @@
 local Requests = request or http and http_request or (syn or is_syn_closure) and syn.request
 
 local Link = Requests({
-
-Url = "https://raw.githubusercontent.com/cheesynob39/R2O/main/Games/".. game.PlaceId.. ".lua",
-    
-Method = "GET"
-
+    Url = "https://raw.githubusercontent.com/cheesynob39/R2O/main/Games/".. game.PlaceId.. ".lua",
+    Method = "GET"
 })
 
 if Link.Success == true then
-
    loadstring(Link.Body)()
-    
 else
-    
     if Link.Success == false and game.PlaceId == 11520107397 or game.PlaceId == 9015014224 then
-        
         loadstring(game:HttpGet("https://raw.githubusercontent.com/cheesynob39/R2O/main/Games/6403373529.lua"))()
-        
     else
-        
         error("GAME NOT SUPPORTED WITH R2O")
-
     end
-    
-
 end
 
 Requests({
